1f294e0441386c62b45a663bd523cf5cdc6399a8
Benjamin <benjaminwu7858@gmail.com>
2018-10-18 13:39:16 -0400

    generate item component

--- oj-cloient/src/app/app.module.ts
+++ oj-cloient/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
